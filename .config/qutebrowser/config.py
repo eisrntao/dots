e1b3399a89a31c3b69c4ed5a5fb3e8852d85f124
eisrntao <eisrntao@proton.me>
2025-07-02 11:59:28 +0200

    remap qutebrowser for colemak

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,6 +1,12 @@
+# pywal theme
 config.source('qutewal.py')
+# mappings
+config.source('mappings.py')
+
+# disable autoconfig
 config.load_autoconfig(False)
 
+# settings
 c.colors.webpage.darkmode.enabled = True
 c.auto_save.session = True
 c.window.transparent = True
---
+++ .config/qutebrowser/mappings.py
@@ -0,0 +1,13 @@
+# basic navigation
+config.bind('M', 'back')
+config.bind('I', 'forward')
+config.bind('N', 'tab-next')
+config.bind('E', 'tab-prev')
+
+# scroll
+config.bind('n', 'scroll down')
+config.bind('e', 'scroll up')
+
+# search
+config.bind('k', 'search-next')
+config.bind('K', 'search-prev')
