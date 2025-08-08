abbe5b96deefcdf60d87f4ce2111f96248408454
eisrntao <eisrntao@proton.me>
2025-08-08 10:38:17 +0200

    catppuccin

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,14 +1,25 @@
+-- Set the colorscheme
 return {
-  {
-    -- Set the colorscheme
-    "rose-pine/neovim",
-    name = "rose-pine",
-    config = function()
-      require("rose-pine").setup({
-        variant = "moon",
-        styles = { transparency = true },
-      })
-      vim.cmd("colorscheme rose-pine")
-    end
-  },
+	-- {
+	-- 	"rose-pine/neovim",
+	-- 	name = "rose-pine",
+	-- 	config = function()
+	-- 		require("rose-pine").setup({
+	-- 			variant = "moon",
+	-- 			styles = { transparency = true },
+	-- 		})
+	-- 		vim.cmd("colorscheme rose-pine")
+	-- 	end,
+	-- },
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				transparent_background = true,
+			})
+			vim.cmd("colorscheme catppuccin-mocha")
+		end,
+	},
 }
