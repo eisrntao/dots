e1ad11f386b079e6bc34f4e48d7eb7f2802a15a5
eisrntao <eisrntao@proton.me>
2026-01-22 22:32:58 +0100

    add session picker script

--- .config/qutebrowser/mappings.py
+++ .config/qutebrowser/mappings.py
@@ -1,18 +1,20 @@
 c.hints.chars = "arstgmneio"
 
 # basic navigation
-config.bind('M', 'back')
-config.bind('I', 'forward')
-config.bind('N', 'tab-next')
-config.bind('E', 'tab-prev')
+config.bind("M", "back")
+config.bind("I", "forward")
+config.bind("N", "tab-next")
+config.bind("E", "tab-prev")
 
 # scroll
-config.bind('n', 'scroll down')
-config.bind('e', 'scroll up')
+config.bind("n", "scroll down")
+config.bind("e", "scroll up")
 
 # search
-config.bind('k', 'search-next')
-config.bind('K', 'search-prev')
+config.bind("k", "search-next")
+config.bind("K", "search-prev")
 
 # misc
-config.bind('z', 'config-cycle colors.webpage.darkmode.enabled')
+config.bind("z", "config-cycle colors.webpage.darkmode.enabled")
+
+config.bind("gs", "spawn --userscript loadsession")
