6be6f2bce992606f8acaa6cef4987f9a65d926b3
eisrntao <eisrntao@proton.me>
2026-01-31 15:23:01 +0100

    update leap

--- .config/nvim/lua/plugins/editing.lua
+++ .config/nvim/lua/plugins/editing.lua
@@ -10,12 +10,19 @@ return {
 	},
 	{
 		-- teleport around
-		"ggandor/leap.nvim",
+		"https://codeberg.org/andyg/leap.nvim",
 		lazy = false,
 		config = function()
-			vim.keymap.set({ "n", "x", "o" }, "s", "<Plug>(leap-forward)")
-			vim.keymap.set({ "n", "x", "o" }, "S", "<Plug>(leap-backward)")
+			vim.keymap.set({ "n", "x", "o" }, "s", "<Plug>(leap)")
 			vim.keymap.set("n", "gs", "<Plug>(leap-from-window)")
+			vim.keymap.set({ "n", "x", "o" }, "S", function()
+				require("leap.treesitter").select({
+					opts = require("leap.user").with_traversal_keys("R", "r"),
+				})
+			end)
+			require("leap").opts.preview = function(ch0, ch1, ch2)
+				return not (ch1:match("%s") or (ch0:match("%a") and ch1:match("%a") and ch2:match("%a")))
+			end
 		end,
 	},
 	-- change brackets easily
