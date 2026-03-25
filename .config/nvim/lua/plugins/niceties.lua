58e675520144c3c31318762445a950e2bad848fe
eisrntao <eisrntao@proton.me>
2026-03-25 19:29:24 +0100

    remove tips

--- .config/nvim/lua/plugins/niceties.lua
+++ .config/nvim/lua/plugins/niceties.lua
@@ -5,29 +5,6 @@ return {
 		lazy = false,
 	},
 	{
-		"saxon1964/neovim-tips",
-		version = "*", -- Only update on tagged releases
-		lazy = false, -- Load on startup for daily tip
-		dependencies = {
-			"MunifTanjim/nui.nvim",
-		},
-		opts = {
-			-- OPTIONAL: Daily tip mode (default: 1)
-			daily_tip = 1, -- 0 = off, 1 = once per day, 2 = every startup
-			bookmark_symbol = " ",
-		},
-		init = function()
-			local map = vim.keymap.set
-			map("n", "<leader>tt", ":NeovimTips<CR>", { desc = " Neovim tips", silent = true })
-			map("n", "<leader>tb", ":NeovimTipsBookmarks<CR>", { desc = " Bookmarked tips", silent = true })
-			map("n", "<leader>tr", ":NeovimTipsRandom<CR>", { desc = " Show random tip", silent = true })
-			map("n", "<leader>ta", ":NeovimTipsAdd<CR>", { desc = "󰗛 Add your Neovim tip", silent = true })
-			map("n", "<leader>te", ":NeovimTipsEdit<CR>", { desc = " Edit your Neovim tips", silent = true })
-			map("n", "<leader>tp", ":NeovimTipsPdf<CR>", { desc = " Open Neovim tips PDF", silent = true })
-			map("n", "<leader>th", ":help neovim-tips<CR>", { desc = "󰮥 Neovim tips help", silent = true })
-		end,
-	},
-	{
 		-- better folds
 		"kevinhwang91/nvim-ufo",
 		dependencies = { "kevinhwang91/promise-async" },
