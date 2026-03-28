c955f688a1709183fc9b191cc806412bec293364
eisrntao <eisrntao@proton.me>
2026-03-28 21:17:05 +0100

    add color schemes

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,25 +1,49 @@
--- Set the colorscheme
+-- set the colorscheme
 return {
-	-- {
-	-- 	"rose-pine/neovim",
-	-- 	name = "rose-pine",
-	-- 	config = function()
-	-- 		require("rose-pine").setup({
-	-- 			variant = "moon",
-	-- 			styles = { transparency = true },
-	-- 		})
-	-- 		vim.cmd("colorscheme rose-pine")
-	-- 	end,
-	-- },
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		priority = 1000,
+		lazy = false,
+		config = function()
+			require("rose-pine").setup({
+				variant = "moon",
+				styles = { transparency = true },
+			})
+			-- vim.cmd("colorscheme rose-pine")
+		end,
+	},
 	{
 		"catppuccin/nvim",
 		name = "catppuccin",
 		priority = 1000,
+		lazy = false,
 		config = function()
 			require("catppuccin").setup({
 				transparent_background = true,
 			})
-			vim.cmd("colorscheme catppuccin-mocha")
+			-- vim.cmd("colorscheme catppuccin-mocha")
+		end,
+	},
+	{
+		"rebelot/kanagawa.nvim",
+		priority = 1000,
+		lazy = false,
+		config = function()
+			require("kanagawa").setup({
+				compile = true, -- compile for perf
+				transparent = true, -- do not set background color
+				colors = {
+					theme = {
+						all = {
+							ui = {
+								bg_gutter = "none",
+							},
+						},
+					},
+				},
+			})
+			vim.cmd("colorscheme kanagawa")
 		end,
 	},
 }
